92ae3035632424194f38ac09cbe73b6316d14768
lcromartie <lyndol.cromartie@bellevuecollege.edu>
2018-11-14 01:58:05 -0800

    modified function-basic.js

--- javascriptTest/function-basic.js
+++ javascriptTest/function-basic.js
@@ -2,10 +2,23 @@ window.onload = () => {
     const simpleFunction = document.getElementById('simple-function')
     const functionCalls = document.getElementById('function-calls');
 
-    function simple() {
-        console.log('simple');
-        simpleFunction.textContent = "simple";
-        functionCalls.onclick - simple;
+    function simpleFunction() {
+        console.log('simple-function');
+        simpleFunction.textContent = "functions should consist of statements designed to perform a single task";
+
+    function functionParameters() {
+        console.log('function-parameters');
+        functionParameters.textContent = "Many functions take parameters"
     }
+    
+    function functionReturn() {
+        console.log('function-returns');
+        functionReturns.textContent = "Many functions return values";
+    }
+
+        
+        functionCalls.onclick = simpleFunction;
+            
+            }
 }
 
